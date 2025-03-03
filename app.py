8c725c26097b6072a5510cf85100f90ea99843fa
Muhammad Ahmed Yaqoob <ra147001y@gmail.com>
2025-03-03 21:15:54 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
-from flask import Flask
+from flask import Flask, send_from_directory
+import os
 app = Flask(__name__)
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 @app.route('/')
 def home():
-    return "Deploying Flask App at Vercel"
+    return send_from_directory(BASE_DIR, 'index.html')
 
 if __name__ == "__main__":
     app.run()
