eb0e9ca727f090c7d24503d4f8f099e243b5bea9
Muhammad Ahmed Yaqoob <ra147001y@gmail.com>
2025-03-03 21:19:54 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,38 @@
 from flask import Flask, send_from_directory
 import os
+
 app = Flask(__name__)
+
+# Get the current directory path
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 @app.route('/')
 def home():
     return send_from_directory(BASE_DIR, 'index.html')
 
-if __name__ == "__main__":
-    app.run()
+@app.route('/about')
+def about():
+    return send_from_directory(BASE_DIR, 'about.html')
+
+@app.route('/blogs')
+def blogs():
+    return send_from_directory(BASE_DIR, 'blogs.html')
+
+@app.route('/contact')
+def contact():
+    return send_from_directory(BASE_DIR, 'contact.html')
+
+@app.route('/rag-chatbot')
+def rag_chatbot():
+    return send_from_directory(BASE_DIR, 'Creating a Rag-Based Chatbot.html')
+
+@app.route('/prompt-engineering')
+def prompt_engineering():
+    return send_from_directory(BASE_DIR, 'prompt engineering.html')
+
+@app.route('/deep-drive')
+def deep_drive():
+    return send_from_directory(BASE_DIR, 'A deep drive.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
